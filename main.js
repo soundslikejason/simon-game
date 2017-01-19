3dc477b47ba8dfb46ca024a927b6d875a93c0747
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-18 21:15:02 -0800

    click events animate and sound

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+  $(".top-left").click(function(event) {
+		flash(".top-left", "#sound-top-left");
+	});
+	$(".top-right").click(function(event) {
+		flash(".top-right", "#sound-top-right");
+	});
+	$(".bottom-left").click(function(event) {
+		flash(".bottom-left", "#sound-bottom-left");
+	});
+	$(".bottom-right").click(function(event) {
+		flash(".bottom-right", "#sound-bottom-right");
+	});
 
+  function flash(btn, sound){
+		$(btn).animate({opacity: 1}, 100);
+		$(btn).animate({opacity: 0.6}, 100);
+    $(sound).get(0).play();
+	}
+});
