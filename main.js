3dbe3888c8a1792f4edf2c6623ecd6615cd457e6
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-22 15:44:55 -0800

    all user stories complete

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 $(document).ready(function() {
   var computer = [];
   var round = 0;
+  var player = [];
+  var correct = false;
+  var strict = false;
 
   $(".btn1").click(function(event) {
 		flash(".btn1", "#sound1");
+    if (round > 0) {
+      check(".btn1");
+    }
 	});
 	$(".btn2").click(function(event) {
 		flash(".btn2", "#sound2");
+    if (round > 0) {
+      check(".btn2");
+    }
 	});
 	$(".btn3").click(function(event) {
 		flash(".btn3", "#sound3");
+    if (round > 0) {
+      check(".btn3");
+    }
 	});
 	$(".btn4").click(function(event) {
 		flash(".btn4", "#sound4");
+    if (round > 0) {
+      check(".btn4");
+    }
 	});
   $("#new").click(function() {
-    newRound();
+    startGame();
+  });
+  $("#strict").click(function(){
+    strict = !strict;
+    $(this).toggleClass("btn-danger");
+    if (strict) {
+      $(this).html("Strict On");
+    } else {
+      $(this).html("Strict Off");
+    }
   });
 
   function flash(btn, sound){
@@ -31,8 +55,14 @@ $(document).ready(function() {
 
   function newRound() {
     round++;
+    if (round > 20){
+			alert("WINNER!!!");
+			newGame();
+		}
+ 		$("#display").val(round + " of 20");
     var random = getRandom(1, 4);
     computer.push(random);
+    player = computer.slice(0);
     animate();
   }
 
@@ -48,4 +78,30 @@ $(document).ready(function() {
       }
     }, 550);
   }
+
+  function check(btn) {
+    var playerPress = player.shift();
+    if (btn === ".btn" + playerPress) {
+      correct = true;
+    } else {
+      if (strict) {
+        alert("Wrong. Start new game!");
+        startGame();
+      } else {
+        alert("Wrong. Try again!");
+        player = computer.slice(0);
+        animate();
+      }
+    }
+    if (player.length === 0 && correct) {
+      newRound();
+    }
+  }
+
+  function startGame() {
+ 		computer = [];
+ 		player = [];
+ 		round = 0;
+ 		newRound();
+ 	}
 });
